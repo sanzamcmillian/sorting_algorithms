99c6bb2ba6911596740b9b7ced8ccbb1494544cd
sanzamcmillian <g18s2658@ru.ac.za>
2024-01-22 12:57:46 +0200

    selection

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,27 @@
 #include "sort.h"
 
 /**
+ *swap - swaps two nodes in a doble linked list
+ *
+ *@a: address of the first node
+ *@b: address of the second node
+ *
+ *Return: void
+ */
+
+void swap(listint_t *a, listint_t *b)
+{
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a:
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
+}
+
+/**
  *selection_sort - an algorithm that sorts a doubly linked list
  *
  *@list: address of pointer to head node
@@ -9,5 +30,26 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	
+	listint_t *i, *j;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+	i = (*list)->next;
+	while(i)
+	{
+		j = i;
+		i = i->next;
+		while (j && j->prev)
+		{
+			if (j->prev->j->n)
+			{
+				swap(j->prev, j);
+				if (!j->prev)
+					*list = j;
+				print_list((const listint_t*)*list);
+			}
+			else
+				j - j->prev;
+		}
+	}
 }
